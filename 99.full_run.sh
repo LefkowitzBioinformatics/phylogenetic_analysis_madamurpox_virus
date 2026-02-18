45c1f19a68e5181b42528af94872b2b2e21179fd
Curtis Hendrickson <curtish@uab.edu>
2026-02-17 23:36:18 -0600

    add convience script to run full protocol

---
+++ 99.full_run.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#
+# run all pipeline steps in order
+#
+# may the force be with you
+
+STEPS=\
+     ./01.build_protein_db.sh \
+     ./02.build_protein_faa.sh \
+     ./03.align.sh \
+     ./04.merge_protein_aligns.sh \
+     ./05.build_tree.iqtree.sh \
+     ./06.build_protein_trees.sh
+
+for STEP in $STEPS; do
+    $STEP
+    if [[ $? -ne 0 ]]; then
+	echo "ERROR[$0] STEP FAILED: $STEP"
+	exit 1
+    fi
+done
+
+cat <<EOF
+# ======================================================================
+# SUCCESS: $0
+# ======================================================================
+EOF
+
+exit 0
